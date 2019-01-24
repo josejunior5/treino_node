68451ca48b2e93fa3c0c004247aa4d78ce293774
Elian Medeiros <elian.medeiros@helpper.com.br>
2019-01-24 17:11:37 -0200

    mergeado com branch 2

--- src/Pessoa.ts
+++ src/Pessoa.ts
@@ -2,13 +2,9 @@ class Pessoa{
     public altura:number;
     public nome:string;
     public idioma:string;
-    public teste:number;
-<<<<<<< HEAD
-    
-=======
+    public teste:number;    
     public teste1:number;
     public teste2:number;
->>>>>>> branch-novo2
 }
 
 export = Pessoa;
